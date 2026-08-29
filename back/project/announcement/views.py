674cc80845521b3a3a559d2861785652429f5a29
CipollinoVP <yurich2002Vadim@gmail.com>
2026-08-29 18:29:40 +0300

    Prototype to run

--- back/project/announcement/views.py
+++ back/project/announcement/views.py
@@ -220,7 +220,8 @@ class GlobalAnnouncementCreateView(APIView):
                 .distinct()
             )
 
-        print(emails)
+        emails_list = list(emails)
+
         requests.post("http://172.17.0.1:8116/", json={
                 "email": [emails],
                 "subject": f"Объявление: {announcement.title}",
